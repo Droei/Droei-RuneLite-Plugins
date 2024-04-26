876f9ca766c91ba94102d18f8932d90906659566
daan <daan.detre@telenet.be>
2024-04-26 12:14:52 +0200

    Refactoring code

--- src/main/java/com/PetTheCapybara/PetTheCapybaraPlugin.java
+++ src/main/java/com/PetTheCapybara/PetTheCapybaraPlugin.java
@@ -1,13 +1,11 @@
 package net.runelite.client.plugins.PetTheCapybara.src.main.java.com.PetTheCapybara;
 
-import com.google.common.collect.ImmutableList;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
-import net.runelite.api.events.MenuOpened;
+import net.runelite.api.events.MenuEntryAdded;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-
 import javax.inject.Inject;
 import java.util.Random;
 
@@ -21,65 +19,25 @@ public class PetTheCapybaraPlugin extends Plugin
 	private Client client;
 	private static final String PET = "Pet";
 
-	private final ImmutableList<String> set = ImmutableList.of(
-			"Trade with", "Attack", "Talk-to", "Examine"
-	);
-
 	@Subscribe
-	public void onMenuOpened(MenuOpened event){
-		MenuEntry[] menuEntries = event.getMenuEntries();
+	public void onMenuEntryAdded(MenuEntryAdded event)
+	{
+		Player local = client.getLocalPlayer();
+		MenuEntry target = event.getMenuEntry();
 
-		for (int i = 0; i < set.size(); i++)
+		int c = event.getIdentifier();
+		if(c == 1057 || c == 1060 || c == 24786 || c == 24784 || c == 24776 || c == 24773 ||
+				c == 24770 || c == 24769 || c == 24783)
 		{
-			boolean addMenuEntry = false;
-			MenuEntry target = null;
-
-			for (MenuEntry menuEntry : menuEntries)
-			{
-				if (menuEntry.getOption().toLowerCase().equals("drop")
-						|| menuEntry.getOption().toLowerCase().equals("destroy"))
-				{
-					return;
-				}
-				else if (menuEntry.getOption().equals(set.get(i)))
-				{
-					addMenuEntry = true;
-					target = menuEntry;
-				}
-
-				if(addMenuEntry){
-					String entityName = target.getTarget();
-					Player local = client.getLocalPlayer();
-
-
-					int c = target.getIdentifier();
-					if(c == 1057 || c == 1060 || c == 24786 || c == 24784 || c == 24776 || c == 24773 ||
-							c == 24770 || c == 24769 || c == 24783)
-					{
-
-						int capyX = target.getNpc().getLocalLocation().getX();
-						int capyY = target.getNpc().getLocalLocation().getY();
-
-						int localX = local.getLocalLocation().getX();
-						int localY = local.getLocalLocation().getY();
-
-						int distanceX = Math.abs(capyX - localX);
-						int distanceY = Math.abs(capyY - localY);
-
-						if (distanceX <= 200 && distanceY <= 200) {
-							client.createMenuEntry(0)
-									.setOption(PET)
-									.setTarget(entityName)
-									.setIdentifier(target.getIdentifier())
-									.onClick(this::PetTheCapy);
-							target.setForceLeftClick(true);
-						} else {
-							local.setOverheadText("Omg a capy I need to get closer to pet it!!!!! ");
-							local.setOverheadCycle(75);
-						}
-					}
-					break;
-				}
+			if (checkDistance(local, target)) {
+				client.createMenuEntry(0)
+						.setOption(PET)
+						.setTarget(target.getTarget())
+						.setIdentifier(target.getIdentifier())
+						.onClick(this::PetTheCapy);
+			} else {
+				local.setOverheadText("Omg a capy I want to get closer to pet it!!!!! ");
+				local.setOverheadCycle(75);
 			}
 		}
 	}
@@ -94,13 +52,21 @@ public class PetTheCapybaraPlugin extends Plugin
 	}
 
 	private String rollRandomLine(){
-
 		CapyLines capyLines = new CapyLines();
+		Random random = new Random();
 
 		final String[] capyList = capyLines.GetCapyLines();
-		Random random = new Random();
-		int index = random.nextInt(capyList.length);
 
-		return capyList[index];
+		return capyList[random.nextInt(capyList.length)];
+	}
+
+	private Boolean checkDistance(Player local, MenuEntry target){
+		int capyX = target.getNpc().getLocalLocation().getX();
+		int capyY = target.getNpc().getLocalLocation().getY();
+
+		int localX = local.getLocalLocation().getX();
+		int localY = local.getLocalLocation().getY();
+
+        return Math.abs(capyX - localX) <= 200 && Math.abs(capyY - localY) <= 200;
 	}
 }
