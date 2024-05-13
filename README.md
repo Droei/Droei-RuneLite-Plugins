4652cf0eeb37a9fa7ef3ad52e85c52b594cb8dfb
daan <daan.detre@telenet.be>
2024-05-13 14:20:58 +0200

    Created skeleton for plugins

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Pet The capybara
-A simple plugin that allows you to pet Capybara's in Runescape
+# Example
+An example greeter plugin
---
+++ src/main/java/be/Droei/example/ExamplePlugin.java
@@ -0,0 +1,35 @@
+package be.droei.example;
+
+import com.google.inject.Provides;
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.ChatMessageType;
+import net.runelite.api.Client;
+import net.runelite.api.GameState;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+@Slf4j
+@PluginDescriptor(
+        name = "Example"
+)
+public class ExamplePlugin extends Plugin
+{
+    @Inject
+    private Client client;
+
+    @Override
+    protected void startUp() throws Exception
+    {
+        log.info("Example started!");
+    }
+
+    @Override
+    protected void shutDown() throws Exception
+    {
+        log.info("Example stopped!");
+    }
+}
